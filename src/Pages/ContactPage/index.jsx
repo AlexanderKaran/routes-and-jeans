cc09c79a1492b0947e0bbd07f03855b8b5484958
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2022-04-10 09:20:42 +0800

    Init commit

---
+++ src/Pages/ContactPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContactPage() {
+  return <h1>I am the contact page</h1>;
+}
+
+export default ContactPage;
---
+++ src/Pages/JeansPage/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function JeansPage() {
+  /**
+   * useParams is a hook (function) that allows us to access the path param. Earlier, when we set the path of the route component that loads the jeans page, we added the path param `: jeans` into the URL.
+   * We can access whatever value is put in this URL section using useParams and then access the value using the same key, `jeans`.
+   */
+  const params = useParams();
+
+  return (
+    <div>
+      <h2>{params.jeans}</h2>
+    </div>
+  );
+}
+
+export default JeansPage;
